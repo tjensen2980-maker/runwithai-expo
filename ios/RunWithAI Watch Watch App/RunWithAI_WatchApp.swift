c6efbf657fd17a2c565e5cf2672ba787ffc84ec9
tjensen2980-maker <tjensen2980@gmail.com>
2026-04-17 14:14:40 +0200

    Watch: standalone app arkitektur som Garmin
    Renamed main app struct and added HealthKit import.

--- ios/RunWithAI Watch Watch App/RunWithAI_WatchApp.swift
+++ ios/RunWithAI Watch Watch App/RunWithAI_WatchApp.swift
@@ -2,16 +2,23 @@
 //  RunWithAI_WatchApp.swift
 //  RunWithAI Watch Watch App
 //
-//  Created by user281474 on 4/16/26.
+//  Standalone Watch app - kører helt selvstændigt som Garmin
 //
 
 import SwiftUI
+import HealthKit
 
 @main
-struct RunWithAI_Watch_Watch_AppApp: App {
+struct RunWithAI_WatchApp: App {
+    
+    @StateObject private var workoutManager = WorkoutManager.shared
+    @StateObject private var connectivityManager = WatchConnectivityManager.shared
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(workoutManager)
+                .environmentObject(connectivityManager)
         }
     }
 }
