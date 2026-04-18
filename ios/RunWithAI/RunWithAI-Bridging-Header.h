9115e20b444206b6641b44e5d924dfa348dc7ede
tjensen2980-maker <tjensen2980@gmail.com>
2026-04-18 22:21:58 +0200

    Add missing RunWithAI-Bridging-Header.h for iOS build

---
+++ ios/RunWithAI/RunWithAI-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+// RunWithAI-Bridging-Header.h
+// RunWithAI
+//
+// React Native bridging headers are managed by CocoaPods
+//
+
+#ifndef RunWithAI_Bridging_Header_h
+#define RunWithAI_Bridging_Header_h
+
+// React Native bridging headers are managed by CocoaPods
+
+#endif /* RunWithAI_Bridging_Header_h */
