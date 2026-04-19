53cf701ad02e768c6ab578f03b1294e337879319
tjensen2980-maker <tjensen2980@gmail.com>
2026-04-19 18:05:37 +0200

    fix: add bundleURL() to AppDelegate.swift so app finds main.jsbundle at runtime
    Added bundleURL method to provide correct JavaScript bundle URL based on build configuration.

--- ios/RunWithAI/AppDelegate.swift
+++ ios/RunWithAI/AppDelegate.swift
@@ -5,10 +5,18 @@ import ReactAppDependencyProvider
 
 @main
 class AppDelegate: RCTAppDelegate {
-    override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-          self.automaticallyLoadReactNativeWindow = true
-          self.moduleName = "main"
-          self.dependencyProvider = RCTAppDependencyProvider()
-          return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
+  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+    self.automaticallyLoadReactNativeWindow = true
+    self.moduleName = "main"
+    self.dependencyProvider = RCTAppDependencyProvider()
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func bundleURL() -> URL? {
+#if DEBUG
+    return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: ".expo/.virtual-metro-entry")
+#else
+    return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+#endif
+  }
 }
