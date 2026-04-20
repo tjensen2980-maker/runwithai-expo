5c6a081b81b85684349d5dfa7b27a8dd42d797b9
Thomas Jensen <tjensen2980@gmail.com>
2026-04-20 16:50:08 +0200

    diag: empty native WatchConnectivity [skip ci]

--- ios/RCTWatchConnectivity.h
+++ ios/RCTWatchConnectivity.h
@@ -1,13 +1,1 @@
-//
-//  RCTWatchConnectivity.h
-//  RunWithAI
-//
-//  Native module that bridges WCSession (Apple Watch) to React Native.
-//
-
-#import <React/RCTBridgeModule.h>
-#import <React/RCTEventEmitter.h>
-
-@interface RCTWatchConnectivity : RCTEventEmitter <RCTBridgeModule>
-
-@end
+// WatchConnectivity disabled for diagnostics
