88d405751f659431ba88d304e8d41567ffcf7df2
Thomas Jensen <thomas.jensen3@ncc.dk>
2026-04-21 11:57:45 +0200

    fix: crash-safe WCSession with lazy activation and try-catch guards

--- ios/RCTWatchConnectivity.mm
+++ ios/RCTWatchConnectivity.mm
@@ -1,27 +1,9 @@
-//
-//  RCTWatchConnectivity.mm
-//  RunWithAI
-//
-//  Native bridge til Apple Watch via WatchConnectivity framework.
-//
-//  Events udsendt til JS:
-//    - WatchMessage             (beskeder fra Watch: { command, data, ... })
-//    - WatchWorkoutComplete     (workout afsluttet på Watch)
-//    - WatchLiveUpdate          (live workout data fra Watch)
-//    - WatchReachabilityChanged ({ isReachable: BOOL })
-//
-//  Exporterede metoder:
-//    - getWatchStatus        -> { isPaired, isWatchAppInstalled, isReachable }
-//    - sendUpdateToWatch     -> sendMessage (kræver reachable)
-//    - transferUserInfo      -> baggrund, garanteret levering
-//    - sendMessage           -> alias for sendUpdateToWatch
-//
-
 #import "RCTWatchConnectivity.h"
 #import <React/RCTLog.h>
 
-@interface RCTWatchConnectivity ()
+@interface RCTWatchConnectivity () <WCSessionDelegate>
 @property (nonatomic, assign) BOOL hasListeners;
+@property (nonatomic, assign) BOOL sessionActivationStarted;
 @end
 
 @implementation RCTWatchConnectivity
@@ -29,41 +11,20 @@
 RCT_EXPORT_MODULE();
 
 + (BOOL)requiresMainQueueSetup {
-  return YES;
-}
-
-- (instancetype)init {
-  self = [super init];
-  if (self) {
-    if ([WCSession isSupported]) {
-      WCSession *session = [WCSession defaultSession];
-      session.delegate = self;
-      [session activateSession];
-      RCTLogInfo(@"[RCTWatchConnectivity] WCSession activated");
-    } else {
-      RCTLogInfo(@"[RCTWatchConnectivity] WCSession not supported on this device");
-    }
-  }
-  return self;
+  return NO;
 }
 
 - (dispatch_queue_t)methodQueue {
   return dispatch_get_main_queue();
 }
 
-#pragma mark - RCTEventEmitter
-
 - (NSArray<NSString *> *)supportedEvents {
-  return @[
-    @"WatchMessage",
-    @"WatchWorkoutComplete",
-    @"WatchLiveUpdate",
-    @"WatchReachabilityChanged"
-  ];
+  return @[@"WatchMessageReceived", @"WatchSessionStateChanged", @"WatchReachabilityChanged"];
 }
 
 - (void)startObserving {
   self.hasListeners = YES;
+  [self ensureSessionActivated];
 }
 
 - (void)stopObserving {
@@ -71,160 +32,134 @@ RCT_EXPORT_MODULE();
 }
 
 - (void)safeSendEvent:(NSString *)name body:(id)body {
-  if (self.hasListeners) {
+  if (!self.hasListeners) { return; }
+  @try {
     [self sendEventWithName:name body:body];
+  } @catch (NSException *exception) {
+    RCTLogWarn(@"RCTWatchConnectivity sendEvent failed: %@", exception.reason);
   }
 }
 
-#pragma mark - Exported methods
+- (void)ensureSessionActivated {
+  if (self.sessionActivationStarted) { return; }
+  self.sessionActivationStarted = YES;
+  @try {
+    if (![WCSession isSupported]) { return; }
+    WCSession *s = [WCSession defaultSession];
+    s.delegate = self;
+    [s activateSession];
+  } @catch (NSException *exception) {
+    RCTLogWarn(@"WCSession activation failed: %@", exception.reason);
+  }
+}
 
-RCT_EXPORT_METHOD(getWatchStatus:(RCTPromiseResolveBlock)resolve
+RCT_EXPORT_METHOD(isSupported:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
-  if (![WCSession isSupported]) {
+  @try {
+    resolve(@([WCSession isSupported]));
+  } @catch (NSException *e) {
+    resolve(@NO);
+  }
+}
+
+RCT_EXPORT_METHOD(getState:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject) {
+  @try {
+    [self ensureSessionActivated];
+    if (![WCSession isSupported]) {
+      resolve(@{@"supported": @NO});
+      return;
+    }
+    WCSession *s = [WCSession defaultSession];
     resolve(@{
-      @"isPaired": @NO,
-      @"isWatchAppInstalled": @NO,
-      @"isReachable": @NO
+      @"supported": @YES,
+      @"activationState": @(s.activationState),
+      @"paired": @(s.isPaired),
+      @"watchAppInstalled": @(s.isWatchAppInstalled),
+      @"reachable": @(s.isReachable)
     });
-    return;
+  } @catch (NSException *e) {
+    resolve(@{@"supported": @NO, @"error": e.reason ?: @"unknown"});
   }
-
-  WCSession *session = [WCSession defaultSession];
-  resolve(@{
-    @"isPaired": @(session.isPaired),
-    @"isWatchAppInstalled": @(session.isWatchAppInstalled),
-    @"isReachable": @(session.isReachable),
-    @"activationState": @(session.activationState)
-  });
-}
-
-RCT_EXPORT_METHOD(sendUpdateToWatch:(NSDictionary *)message
-                  resolver:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject) {
-  if (![WCSession isSupported]) {
-    reject(@"not_supported", @"WCSession not supported on this device", nil);
-    return;
-  }
-
-  WCSession *session = [WCSession defaultSession];
-
-  if (!session.isReachable) {
-    reject(@"not_reachable", @"Watch is not currently reachable", nil);
-    return;
-  }
-
-  [session sendMessage:message
-          replyHandler:^(NSDictionary<NSString *,id> * _Nonnull reply) {
-    resolve(reply ?: @{@"success": @YES});
-  }
-          errorHandler:^(NSError * _Nonnull error) {
-    reject(@"send_failed", error.localizedDescription, error);
-  }];
 }
 
 RCT_EXPORT_METHOD(sendMessage:(NSDictionary *)message
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
-  // Alias for sendUpdateToWatch
-  if (![WCSession isSupported]) {
-    reject(@"not_supported", @"WCSession not supported", nil);
-    return;
+  @try {
+    [self ensureSessionActivated];
+    if (![WCSession isSupported]) { reject(@"not_supported", @"WCSession not supported", nil); return; }
+    WCSession *s = [WCSession defaultSession];
+    if (!s.isReachable) { reject(@"not_reachable", @"Watch not reachable", nil); return; }
+    [s sendMessage:message
+      replyHandler:^(NSDictionary<NSString *,id> *reply) { resolve(reply ?: @{@"success":@YES}); }
+      errorHandler:^(NSError *error) { reject(@"send_failed", error.localizedDescription, error); }];
+  } @catch (NSException *e) {
+    reject(@"send_exception", e.reason ?: @"unknown", nil);
   }
-  WCSession *session = [WCSession defaultSession];
-  if (!session.isReachable) {
-    // Fallback: transferUserInfo hvis watch ikke er reachable
-    [session transferUserInfo:message];
-    resolve(@{@"queued": @YES});
-    return;
+}
+
+RCT_EXPORT_METHOD(updateApplicationContext:(NSDictionary *)context
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject) {
+  @try {
+    [self ensureSessionActivated];
+    if (![WCSession isSupported]) { reject(@"not_supported", @"WCSession not supported", nil); return; }
+    WCSession *s = [WCSession defaultSession];
+    NSError *err = nil;
+    BOOL ok = [s updateApplicationContext:context error:&err];
+    if (ok) { resolve(@{@"success":@YES}); }
+    else { reject(@"context_failed", err.localizedDescription ?: @"unknown", err); }
+  } @catch (NSException *e) {
+    reject(@"context_exception", e.reason ?: @"unknown", nil);
   }
-  [session sendMessage:message
-          replyHandler:^(NSDictionary<NSString *,id> * _Nonnull reply) {
-    resolve(reply ?: @{@"success": @YES});
-  }
-          errorHandler:^(NSError * _Nonnull error) {
-    reject(@"send_failed", error.localizedDescription, error);
-  }];
 }
 
 RCT_EXPORT_METHOD(transferUserInfo:(NSDictionary *)userInfo
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
-  if (![WCSession isSupported]) {
-    reject(@"not_supported", @"WCSession not supported", nil);
-    return;
+  @try {
+    [self ensureSessionActivated];
+    if (![WCSession isSupported]) { reject(@"not_supported", @"WCSession not supported", nil); return; }
+    WCSession *s = [WCSession defaultSession];
+    [s transferUserInfo:userInfo];
+    resolve(@{@"success":@YES});
+  } @catch (NSException *e) {
+    reject(@"transfer_exception", e.reason ?: @"unknown", nil);
   }
-
-  WCSession *session = [WCSession defaultSession];
-  WCSessionUserInfoTransfer *transfer = [session transferUserInfo:userInfo];
-
-  resolve(@{
-    @"transferring": @(transfer != nil),
-    @"success": @YES
-  });
 }
 
 #pragma mark - WCSessionDelegate
 
-- (void)session:(WCSession *)session
-activationDidCompleteWithState:(WCSessionActivationState)activationState
-          error:(NSError *)error {
-  if (error) {
-    RCTLogError(@"[RCTWatchConnectivity] Activation error: %@", error.localizedDescription);
-  } else {
-    RCTLogInfo(@"[RCTWatchConnectivity] Activation state: %ld", (long)activationState);
-  }
+- (void)session:(WCSession *)session activationDidCompleteWithState:(WCSessionActivationState)activationState error:(NSError *)error {
+  [self safeSendEvent:@"WatchSessionStateChanged" body:@{@"state": @(activationState), @"error": error.localizedDescription ?: [NSNull null]}];
 }
 
-- (void)sessionDidBecomeInactive:(WCSession *)session {
-  RCTLogInfo(@"[RCTWatchConnectivity] Session became inactive");
-}
+- (void)sessionDidBecomeInactive:(WCSession *)session {}
 
 - (void)sessionDidDeactivate:(WCSession *)session {
-  RCTLogInfo(@"[RCTWatchConnectivity] Session deactivated, re-activating");
-  [[WCSession defaultSession] activateSession];
+  @try { [[WCSession defaultSession] activateSession]; } @catch (NSException *e) {}
 }
 
 - (void)sessionReachabilityDidChange:(WCSession *)session {
-  [self safeSendEvent:@"WatchReachabilityChanged" body:@{
-    @"isReachable": @(session.isReachable)
-  }];
+  [self safeSendEvent:@"WatchReachabilityChanged" body:@{@"reachable": @(session.isReachable)}];
 }
 
 - (void)session:(WCSession *)session didReceiveMessage:(NSDictionary<NSString *,id> *)message {
-  [self routeIncomingMessage:message];
+  [self safeSendEvent:@"WatchMessageReceived" body:message];
 }
 
-- (void)session:(WCSession *)session
-  didReceiveMessage:(NSDictionary<NSString *,id> *)message
-       replyHandler:(void (^)(NSDictionary<NSString *,id> * _Nonnull))replyHandler {
-  [self routeIncomingMessage:message];
-  replyHandler(@{@"received": @YES});
+- (void)session:(WCSession *)session didReceiveMessage:(NSDictionary<NSString *,id> *)message replyHandler:(void (^)(NSDictionary<NSString *,id> *))replyHandler {
+  [self safeSendEvent:@"WatchMessageReceived" body:message];
+  replyHandler(@{@"ack": @YES});
 }
 
 - (void)session:(WCSession *)session didReceiveUserInfo:(NSDictionary<NSString *,id> *)userInfo {
-  [self routeIncomingMessage:userInfo];
+  [self safeSendEvent:@"WatchMessageReceived" body:userInfo];
 }
 
-- (void)session:(WCSession *)session
-didReceiveApplicationContext:(NSDictionary<NSString *,id> *)applicationContext {
-  [self routeIncomingMessage:applicationContext];
-}
-
-#pragma mark - Helpers
-
-- (void)routeIncomingMessage:(NSDictionary *)message {
-  if (!message) return;
-
-  NSString *type = message[@"type"];
-
-  if ([type isEqualToString:@"WORKOUT_COMPLETE"]) {
-    [self safeSendEvent:@"WatchWorkoutComplete" body:message];
-  } else if ([type isEqualToString:@"LIVE_UPDATE"] || [type isEqualToString:@"RUN_UPDATE"]) {
-    [self safeSendEvent:@"WatchLiveUpdate" body:message];
-  } else {
-    // Alt andet routes som generel WatchMessage (inkl. commands)
-    [self safeSendEvent:@"WatchMessage" body:message];
-  }
+- (void)session:(WCSession *)session didReceiveApplicationContext:(NSDictionary<NSString *,id> *)applicationContext {
+  [self safeSendEvent:@"WatchMessageReceived" body:applicationContext];
 }
 
 @end
