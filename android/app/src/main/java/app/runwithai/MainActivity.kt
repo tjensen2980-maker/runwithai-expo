a73ddcf64de93358e36d3aa234a20f3d739bf67d
Jensen Thomas (Admin) <zadm.dkthojec@adroot.net>
2026-06-10 21:56:38 +0200

    fix(android): register HealthConnect permission delegate to prevent startup crash

--- android/app/src/main/java/app/runwithai/MainActivity.kt
+++ android/app/src/main/java/app/runwithai/MainActivity.kt
@@ -9,6 +9,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnable
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 import expo.modules.ReactActivityDelegateWrapper
+import dev.matinzd.healthconnect.permissions.HealthConnectPermissionDelegate
 
 class MainActivity : ReactActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +18,7 @@ class MainActivity : ReactActivity() {
     // This is required for expo-splash-screen.
     setTheme(R.style.AppTheme);
     super.onCreate(null)
+     HealthConnectPermissionDelegate.setPermissionDelegate(this)
   }
 
   /**
