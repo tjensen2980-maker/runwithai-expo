b9207c8d05427b207b028f67c81e5e8f2f31a496
tjensen2980-maker <tjensen2980@gmail.com>
2026-06-16 21:12:57 +0200

    feat(ios): add widget target config for Live Activity (2/4)

---
+++ targets/widget/expo-target.config.js
@@ -0,0 +1,10 @@
+/** @type {import('@bacons/apple-targets').Config} */
+module.exports = {
+  type: "widget",
+  icon: "../../assets/icon.png",
+  colors: {
+    $accent: "#c8ff00",
+    $widgetBackground: "#000000",
+  },
+  deploymentTarget: "16.1",
+};
