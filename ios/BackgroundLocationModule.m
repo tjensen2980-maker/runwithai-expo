16439458c2bcbee5b7c73766644f2019d68efe4b
tjensen2980-maker <tjensen2980@gmail.com>
2026-06-28 17:46:19 +0200

    Implement BackgroundLocationModule in Objective-C
    Add Objective-C bridge for BackgroundLocationModule to expose functionality to React Native.

---
+++ ios/BackgroundLocationModule.m
@@ -0,0 +1,23 @@
+// BackgroundLocationModule.m
+// Objective-C bridge der eksponerer BackgroundLocationModule.swift til React Native.
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+
+@interface RCT_EXTERN_MODULE(BackgroundLocationModule, RCTEventEmitter)
+
+RCT_EXTERN_METHOD(start:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(stop:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(isTracking:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+
++ (BOOL)requiresMainQueueSetup {
+  return NO;
+}
+
+@end
